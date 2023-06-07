a912fa6fa44ec62f1f36b2bad1947912313e3a73
feuvpi <fredvpg@gmail.com>
2023-06-07 20:16:48 -0300

    face tracking corrected

--- App.js
+++ App.js
@@ -1,33 +1,73 @@
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera'
+import { useState } from 'react'
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import * as FaceDetector from 'expo-face-detector'
 
-export default function App() {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
+export default function App () {
+  const [type, setType] = useState(CameraType.back)
+  const [permission, requestPermission] = Camera.useCameraPermissions()
+
+  const [face, setFace] = useState(null)
 
   if (!permission) {
     // Camera permissions are still loading
-    return <View />;
+    return <View />
   }
 
   if (!permission.granted) {
     // Camera permissions are not granted yet
     return (
       <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <Text style={{ textAlign: 'center' }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title='grant permission' />
       </View>
-    );
+    )
   }
 
-  function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  const handleFacesDetected = ({ faces }) => {
+    if(faces && faces.length > 0){
+      setFace(faces[0]);
+    }
+  }
+
+  function toggleCameraType () {
+    setType(current =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    )
   }
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        onFacesDetected={handleFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.FaceDetectorMode.fast,
+          detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+          runClassifications: FaceDetector.FaceDetectorClassifications.none,
+          minDetectionInterval: 100,
+          tracking: true
+        }}
+      >
+        {face && (
+          <View
+          key={face.faceID}
+            style={[
+              styles.face,
+              {
+                width: face.bounds.size.width,
+                height: face.bounds.size.height,
+                left: face.bounds.origin.x,
+                top: face.bounds.origin.y
+              }
+            ]}
+            
+          ></View>
+        )}
+
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
@@ -35,31 +75,43 @@ export default function App() {
         </View>
       </Camera>
     </View>
-  );
+  )
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   camera: {
-    flex: 1,
+    flex: 1
+  },
+  face: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    padding: 10,
+    borderWidth: 3,
+    borderRadius: 20,
+    borderColor: 'red',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
   },
   buttonContainer: {
     flex: 1,
     flexDirection: 'row',
     backgroundColor: 'transparent',
-    margin: 64,
+    margin: 64
   },
   button: {
     flex: 1,
     alignSelf: 'flex-end',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: 'white',
-  },
-});
+    color: 'white'
+  }
+})
